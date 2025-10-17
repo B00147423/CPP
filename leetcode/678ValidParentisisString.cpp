d1fb659eca7ea39151af13a3748eb1feec620fb2
b00147423 <b00147423@mytudublin.ie>
2025-10-17 19:31:29 +0100

    678. Valid Parenthesis String leetcode

---
+++ leetcode/678ValidParentisisString.cpp
@@ -0,0 +1,27 @@
+#include <string>
+using namespace std;
+class Solution {
+public:
+    bool checkValidString(string s) {
+        int low = 0;
+        int high = 0;
+
+        for(char c : s){
+            if(c == '('){
+                low++;
+                high++;
+            }else if(c == ')'){
+                low--;
+                high--;
+            }else{
+                low--;
+                high++;
+            }
+            low = max(low, 0);
+            if (high < 0){
+                return false;
+            }
+        }
+        return low ==0;
+    }
+};
