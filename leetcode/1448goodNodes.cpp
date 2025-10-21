d58ffe0c7bb77d0cfd57244fd4f3dd034aec5200
b00147423 <b00147423@mytudublin.ie>
2025-10-21 16:34:57 +0100

    1448. Count Good Nodes in Binary Tree

---
+++ leetcode/1448goodNodes.cpp
@@ -0,0 +1,33 @@
+#include <algorithm>
+using namespace std;
+// Definition for a binary tree node.
+ struct TreeNode {
+     int val;
+     TreeNode *left;
+     TreeNode *right;
+     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ };
+
+class Solution {
+public:
+    int goodNodes(TreeNode* root) {
+        if (!root) return 0;
+        return dfs(root, root->val);
+    }
+private: 
+    int dfs(TreeNode* node, int maxSoFar) {
+        if (!node) return 0;
+        int count;
+        if (node->val >= maxSoFar) {
+            count = 1;
+        } else {
+            count = 0;
+        }
+        maxSoFar = max(maxSoFar, node->val);
+        count+=dfs(node->left, maxSoFar);
+        count+=dfs(node->right, maxSoFar);
+        return count;
+    }
+};
