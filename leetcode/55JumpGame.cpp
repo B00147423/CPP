fce5857d45901f307b39500b1c9b8cc845415e91
B00147423 <b00147423@mytudublin.ie>
2025-10-26 10:44:19 +0000

    55. Jump Game  leetcode

---
+++ leetcode/55JumpGame.cpp
@@ -0,0 +1,18 @@
+#include <vector>
+#include <iostream>
+
+class Solution {
+public:
+    bool canJump(std::vector<int>& nums) {
+        int goal = nums.size()-1;
+
+        for(int i = nums.size()-2; i >=0; i -- )
+            if(i + nums[i] >= goal)
+            {
+                goal = i;
+            }
+        return goal == 0;
+    }
+};
+
+
