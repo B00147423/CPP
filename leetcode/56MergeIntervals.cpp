194b9524bb20df2276a2c35225e569ff7e8d9b23
B00147423 <b00147423@mytudublin.ie>
2025-11-15 19:34:29 +0000

    leetcode 56 2nd solution fix

--- leetcode/56MergeIntervals.cpp
+++ leetcode/56MergeIntervals.cpp
@@ -23,3 +23,46 @@ public:
         return result;
     }
 };
+
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+
+        // sort intervals by first element
+        sort(intervals.begin(), intervals.end());
+
+        // create output list
+        vector<vector<int>> output;
+
+        // put the first interval into output
+        output.push_back(intervals[0]);
+
+        // go through each interval
+        for (auto& interval : intervals) {
+
+            // get current interval's start
+            int start = interval[0];
+
+            // get current interval's end
+            int end = interval[1];
+
+            // get the last interval's end inside output
+            int lastEnd = output.back()[1];
+
+            // check if start is small enough to update last end
+            if (start <= lastEnd) {
+
+                // replace the last interval's end with the bigger one
+                output.back()[1] = max(lastEnd, end);
+
+            } else {
+
+                // add a new interval to output
+                output.push_back({start, end});
+            }
+        }
+
+        // return output list
+        return output;
+    }
+};
