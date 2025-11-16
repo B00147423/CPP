ce8f6f64f8da79852337f48423bc83638c90b019
B00147423 <b00147423@mytudublin.ie>
2025-11-16 21:29:08 +0000

    NEETCODE VERSION added

--- leetcode/253MeetingRoomsII.cpp
+++ leetcode/253MeetingRoomsII.cpp
@@ -19,3 +19,35 @@ public:
         return minHeap.size();
     }
 };
+
+//NEETCODE VERSION 
+
+
+ //Definition of Interval:
+ class Interval {
+ public:
+     int start, end;
+     Interval(int start, int end) {
+         this->start = start;
+         this->end = end;
+     }
+
+
+// --------------------------NEETCODE VERSION----------------------------------------
+public:
+    int minMeetingRooms(std::vector<Interval>& intervals) {
+
+        sort(intervals.begin(), intervals.end(),
+            [](const Interval& a, const Interval& b){
+                return a.start < b.start;
+            });
+            std::priority_queue<int, std::vector<int>, std::greater<int>> minHeap;
+            for (const auto& interval : intervals) {
+                if (!minHeap.empty() && minHeap.top() <= interval.start) {
+                    minHeap.pop();
+                }
+                minHeap.push(interval.end);
+            }
+        return minHeap.size();
+    }
+};
