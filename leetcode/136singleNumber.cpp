b55ddcf4e4433674667e2b64abb70be210b8ec81
B00147423 <b00147423@mytudublin.ie>
2025-11-21 10:59:24 +0000

    136. Single Number

---
+++ leetcode/136singleNumber.cpp
@@ -0,0 +1,14 @@
+#include <vector>
+class Solution {
+public:
+    int singleNumber(std::vector<int>& nums) {
+
+
+        int result = 0;
+        for (int n : nums)
+        {     
+            result ^= n;
+        }
+        return result;
+    }
+};
