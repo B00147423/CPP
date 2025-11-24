d4d468aa67026add6c1f3b96f01795b2801ba8c0
B00147423 <b00147423@mytudublin.ie>
2025-11-24 11:57:10 +0000

    1018. Binary Prefix Divisible By 5 leetcode

---
+++ leetcode/1018prefixesDivBy5.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    std::vector<bool> prefixesDivBy5(std::vector<int>& nums) {
+
+        std::vector<bool> result;
+        int current = 0;
+        
+
+        for(int i = 0; i < nums.size(); i++){
+            current = (current * 2 + nums[i]) % 5;
+            if(current == 0){
+                result.push_back(true);
+            } else {
+                result.push_back(false);
+            }
+        }   
+        return result;
+    }
+};
+
