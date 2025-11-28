a0164d4781003f44728de49c8ba4c48567aab794
B00147423 <b00147423@mytudublin.ie>
2025-11-28 00:08:35 +0000

    Count Elements Greater Than Previous Average   hackerrank problem

---
+++ HackerRank/countResponseTimeRegressions.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+#include <stack>
+#include <string>
+#include <algorithm>
+#include <cctype>
+
+
+/*
+ * Complete the 'countResponseTimeRegressions' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts INTEGER_ARRAY responseTimes as parameter.
+ */
+
+int countResponseTimeRegressions(std::vector<int> responseTimes) {
+    int n = responseTimes.size();
+    int count = 0;
+    
+    if(n <= 1)return 0;
+
+    double sum = responseTimes[0];
+    for(int i = 1; i < n; i++){
+        double avarage = sum / i;
+        if(responseTimes[i] > avarage) count++;
+    
+        sum+= responseTimes[i];
+    }
+    return count;
+}
