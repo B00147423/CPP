522d5a97e8f4517f630e593d19ba3b359086b0eb
beka betsunaidze <b00147423@mytudublin.ie>
2026-01-03 23:39:09 +0000

    1079. Letter Tile Possibilities

---
+++ leetcode/079LetterTilePossibilities.cpp
@@ -0,0 +1,30 @@
+#include <string>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    int numTilePossibilities(string tiles) {
+        vector<int> freq(26, 0);
+
+        for (char c : tiles) {
+            freq[c - 'A']++;
+        }
+        return backtrack(freq);
+    }
+
+    int backtrack(vector<int>& freq) {
+        int count = 0;
+
+        for (int i = 0; i < 26; i++) {
+            if(freq[i] == 0) continue;
+            
+            freq[i]--;
+            count++;
+            count+= backtrack(freq);
+            freq[i]++;
+        }
+        return count;
+    }
+    
+};
