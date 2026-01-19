c1bf2054834de4545d6205dd23937b53bda6a0d5
beka betsunaidze <b00147423@mytudublin.ie>
2026-01-19 21:44:17 +0000

    111. Minimum Depth of Binary Tree

---
+++ leetcode/111minDepth.cpp
@@ -0,0 +1,36 @@
+// Definition for a binary tree node.
+#include <cstddef>
+#include <algorithm>
+using namespace std;
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+
+class Solution {
+public:
+    int minDepth(TreeNode* root) {
+        return dfs(root);
+    }
+    
+    int dfs(TreeNode* root) {
+        if (root == NULL) {
+            return 0;
+        }   
+        // If only one of child is non-null, then go into that recursion.
+        if (!root->left) {
+            return 1 + dfs(root->right);
+        } else if (!root->right) {
+            return 1 + dfs(root->left);
+        }
+
+        // Both children are non-null, hence call for both children.
+        return 1 + min(dfs(root->left), dfs(root->right));
+    }
+
+};
