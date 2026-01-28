057e371e5ef2db54a7eab68148acc07905c18afa
beka betsunaidze <b00147423@mytudublin.ie>
2026-01-28 00:29:30 +0000

    FIUZXED REDID IT

--- leetcode/31NextPermutation.cpp
+++ leetcode/31NextPermutation.cpp
@@ -4,22 +4,63 @@ using namespace std;
 class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
-        int i = nums.size()-1; // 3
-        while(i > 0 && nums[i-1] >= nums[i]){ 
-            i--;
-        }
 
-        if(i == 0){
+        int n = nums.size();
+
+
+        //finding the pivot point to
+        int pivot = -1;
+        for(int i = n-2; i >=  0; i--){
+            if(nums[i] < nums[i+1]){
+                pivot = 1;
+            }
+        }
+        // we didnt find pivot because it doesnt exitst
+        // reverse the array
+        if(pivot == -1){
             reverse(nums.begin(), nums.end());
             return;
         }
-        int j = nums.size()-1;
-        while(j >= i && nums[j] <= nums[i-1]){
-            j--;
-        } 
-        swap(nums[i-1], nums[j]);
-        reverse(nums.begin()+i, nums.end());
+
+        // find the element from the right that
+        // is greater than pivot    
+        for(int i = n-1; i >= 0; i--){
+            if (nums[i] > nums[pivot]) {
+                swap(nums[i],nums[pivot]);
+            }
+            break;
+        }
+
+        // Reverse the elements from pivot + 1 to the 
+        // end to get the next permutation
+        reverse(nums.begin()+ pivot+1, nums.end());
     }
 };
 
+/*
+arr = [1, 2, 3]
+n   = 3
 
+FIRST LOOP (find pivot)
+----------------------
+i = 1        arr[1] < arr[2]     2 < 3     true
+pivot = 1
+
+SECOND LOOP (find swap)
+----------------------
+i = 2        arr[2] > arr[1]     3 > 2     true
+swap(2, 1)
+
+arr = [1, 3, 2]
+
+REVERSE SUFFIX
+--------------
+start = pivot + 1 = 2
+end   = 2
+
+(no change)
+
+FINAL
+-----
+[1, 3, 2]
+*/
