ff12ee7500bd8464a2363e16989e1a57adebf258
beka betsunaidze <b00147423@mytudublin.ie>
2026-01-31 00:26:33 +0000

    dry run fix

--- leetcode/160IntersectionOfTwoLinkedLists.cpp
+++ leetcode/160IntersectionOfTwoLinkedLists.cpp
@@ -34,3 +34,32 @@ public:
         return tempA;
     }
 };
+// Time Complexity: O(m + n) where m and n are the lengths of the two linked lists
+// Space Complexity: O(1)
+
+/*
+dry run: 
+List A:     4 -> 1 \
+                   -> 8 -> 4 -> 5
+List B: 5 -> 6 ->1 /
+
+start:
+A: 4        B: 5
+
+move:
+A: 1        B: 6
+A: 8        B: 1
+A: 4        B: 8
+A: 5        B: 4
+A: null     B: 5
+
+reset:
+A: 5        B: null
+
+move:
+A: 6        B: 4
+A: 1        B: 1
+A: 8        B: 8   ← SAME NODE
+
+
+*/
