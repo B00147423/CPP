73ef65960b3fa718ebfa4651a3c0125574e6fce7
beka betsunaidze <b00147423@mytudublin.ie>
2026-02-07 21:54:12 +0000

    Leetcode 739. Daily Temperatures

---
+++ leetcode/python/739DailyTemperatures.py
@@ -0,0 +1,18 @@
+from typing import List
+
+
+class Solution:
+    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        
+        n = len(temperatures)
+        result = [0] * n
+        stack = []
+
+        for i, temp in enumerate(temperatures):
+            while stack and temp > temperatures[stack[-1]]:
+                previous_day = stack.pop()
+                result[previous_day] = i - previous_day
+            stack.append(i)
+
+
+        return result
