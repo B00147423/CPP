9296fbc2abf8c642e13ca849e0256b52c80fc1be
beka betsunaidze <b00147423@mytudublin.ie>
2026-03-17 01:29:59 +0000

    Leetcode 78. Subsets

---
+++ leetcode/python/78Subsets.py
@@ -0,0 +1,24 @@
+# 78. Subsets
+
+from typing import List
+
+
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        res = []
+
+        def backtrack(start, subset):
+            res.append(subset[:])
+
+            for i in range(start, len(nums)):
+                subset.append(nums[i])
+                backtrack(i + 1, subset)
+                subset.pop()
+
+        backtrack(0, [])
+        return res
+
+if __name__ == "__main__":
+    s = Solution()
+    print(s.subsets([1, 2, 3]))
+    
