94e5025744ad7bdd0b2557b6385c1dde5c952d07
beka betsunaidze <b00147423@mytudublin.ie>
2026-03-17 22:07:59 +0000

    Leetcode 90. Subsets II

---
+++ leetcode/python/90.Subsets2.py
@@ -0,0 +1,23 @@
+# 90. Subsets II
+# Given an integer array nums that may contain duplicates, return all possible subsets (the power set).
+# The solution set must not contain duplicate subsets. The subsets can be returned in any order.
+from typing import List
+class Solution:
+    def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
+        
+
+        res = []
+        nums.sort()
+        self.backtrack(nums, 0, [], res)
+        return res
+
+    def backtrack(self, nums, start, subset, res):
+        res.append(subset[:])
+
+        for i in range(start, len(nums)):
+            if i > start and nums[i] == nums[i - 1]:
+                continue
+
+            subset.append(nums[i])
+            self.backtrack(nums, i + 1, subset, res)
+            subset.pop()
