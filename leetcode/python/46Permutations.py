9850b7abdbeaed3fed041099054abfc4aa0e172f
beka betsunaidze <b00147423@mytudublin.ie>
2026-03-18 23:10:48 +0000

    Leetcode 45. Permutations

---
+++ leetcode/python/46Permutations.py
@@ -0,0 +1,25 @@
+
+# 45. Permutations
+from typing import List
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        self.res = []
+        self.backtrack(nums, [])
+        return self.res
+        
+    def backtrack(self, nums, perm):
+        
+        if len(perm) == len(nums):
+            self.res.append(perm[:])
+            return
+
+        for n in nums:
+            if n in perm:
+                continue
+            perm.append(n)
+            self.backtrack(nums, perm)
+            perm.pop()
+
+
+print(Solution().permute([1,2,3]))
+print(Solution().permute([[7]]))
