c2710ab61a4c213d3512093f48f1d09b79c76d8a
beka betsunaidze <b00147423@mytudublin.ie>
2026-03-20 23:58:49 +0000

    Leetcode 39. Combination Sum

---
+++ leetcode/python/39CombinationSum.py
@@ -0,0 +1,27 @@
+# 39. Combination Sum
+
+from typing import List
+
+class Solution:
+    def combinationSum(self, nums: List[int], target: int) -> List[List[int]]:
+        
+        res = []
+        self.backtrack(nums, 0, [], target, res)
+        return res
+
+    def backtrack(self, nums, start, subset, remaining, res):
+        if remaining == 0:
+            res.append(subset[:])
+            return
+
+        for i in range(start, len(nums)):
+            if nums[i] > remaining:
+                continue
+
+            subset.append(nums[i])
+            self.backtrack(nums, i, subset, remaining - nums[i], res)
+            subset.pop()
+
+if __name__ == "__main__":
+    s = Solution()
+    print(s.combinationSum([2, 3, 6, 7], 7))
