8a37714ee47f299d1a22fa5007b8f145962378f6
beka betsunaidze <b00147423@mytudublin.ie>
2026-04-05 00:23:47 +0100

    Leetcode332. Reconstruct Itinerary

---
+++ leetcode/python/reconstructFlightPath.py
@@ -0,0 +1,28 @@
+# 332. Reconstruct Itinerary
+from collections import defaultdict
+from typing import List
+class Solution:
+    def findItinerary(self, tickets: List[List[str]]) -> List[str]:
+
+        graph = defaultdict(list)
+        for from_i, to_i in tickets:
+            graph[from_i].append(to_i)
+            print(graph)
+        
+        for key in graph:
+            graph[key].sort()
+            print(graph)
+
+        res = []
+
+
+        def dfs(node):
+
+            while graph[node]:
+                next_dest = graph[node].pop(0)
+                dfs(next_dest)
+            res.append(node)
+
+        dfs('JFK')
+        res.reverse()
+        return res
