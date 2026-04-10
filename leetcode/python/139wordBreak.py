6371c87632cd006b17edb9676de814ddcf6c8f22
beka betsunaidze <b00147423@mytudublin.ie>
2026-04-10 20:47:44 +0100

    leetcode questinos139, 236, 261, 355, 647 and 416

---
+++ leetcode/python/139wordBreak.py
@@ -0,0 +1,23 @@
+# 139. Word Break
+from typing import List
+
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+        # We can use a dictionary to store the results of 
+        # previously computed indices
+        memo = {len(s) : True}
+        # index is the starting point of the substring we want to check
+        def dfs(index): 
+            # If we have already computed the result for this index, return it
+            if index in memo: 
+                return memo[index]
+            # Check if the substring starting from index can be 
+            # segmented into words in the dictionary
+            for word in wordDict:
+                if s[index : index + len(word)] == word:
+                    if dfs(index + len(word)):
+                        memo[index] = True
+                        return True
+            memo[index] = False # Store the result
+            return False
+        return dfs(0)
