6422b0dc0b856dea96e271ff71a2ff6a57d2c4d4
beka betsunaidze <b00147423@mytudublin.ie>
2026-04-17 22:18:46 +0100

    Leetcode 55. Jump Game

---
+++ leetcode/python/55jumpGame.py
@@ -0,0 +1,13 @@
+# 55. Jump Game
+from typing import List
+class Solution:
+    def canJump(self, nums: List[int]) -> bool:
+        max_jump = 0
+
+        for i in range(len(nums)):
+            if i > max_jump:
+                return False
+
+            max_jump = max(max_jump, i + nums[i])
+
+        return True
