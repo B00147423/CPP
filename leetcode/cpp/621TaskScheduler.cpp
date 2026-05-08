2fb3bdbf4766a657d3a90a38ade028c125af3454
beka betsunaidze <b00147423@mytudublin.ie>
2026-05-09 00:27:06 +0100

    621. Task Scheduler

---
+++ leetcode/cpp/621TaskScheduler.cpp
@@ -0,0 +1,61 @@
+//767. Reorganize String
+#include <string>
+#include <queue>
+#include <unordered_map>
+
+using namespace std;
+class Solution {
+public:
+    int leastInterval(vector<char>& tasks, int n) {
+        if (tasks.empty()) return 0;
+
+        unordered_map<char, int> freq;
+        priority_queue<int> pq;
+
+        for (char task : tasks) {
+            freq[task]++;
+        }
+
+        for (auto it : freq) {
+            pq.push(it.second);
+        }
+
+        int time = 0;
+
+        while(pq.size() > 1)
+        {
+            vector<int> temp;
+            int cycle = n + 1;
+
+            while (cycle > 0 && !pq.empty()) {
+                int count = pq.top();
+                pq.pop();
+
+                count--;
+                time++;
+                cycle--;
+
+                if (count > 0) {
+                    temp.push_back(count);
+                }
+            }
+        
+            for (int count : temp) 
+            {
+                pq.push(count);
+            }
+
+            if(!pq.empty())
+            {
+                time += cycle;//idle slots
+            }
+        }
+
+        return time;
+    }
+};
+
+/*
+
+
+*/
