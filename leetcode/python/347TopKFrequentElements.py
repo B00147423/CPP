07f2f76870523413c6ccf306c4d65c5cd53c4d31
beka betsunaidze <b00147423@mytudublin.ie>
2026-05-30 00:09:11 +0100

    leetcode 347. Top K Frequent Elements

--- leetcode/python/347TopKFrequentElements.py
+++ leetcode/python/347TopKFrequentElements.py
@@ -1,5 +1,31 @@
 # 347. Top K Frequent Elements
 from typing import List
+from collections import defaultdict
+import heapq
+
+
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        n = len(nums)
+        freq = defaultdict(int)
+
+        for num in nums:
+            freq[num] += 1
+
+        heap = []
+
+        for num, count in freq.items():
+            heapq.heappush(heap, (count, num))
+
+            if len(heap) > k:
+                heapq.heappop(heap)
+
+        res = []
+        
+        for cnt, num in heap:
+            res.append(num)
+        return [num for count, num in heap]
+
 
 
 class Solution:
